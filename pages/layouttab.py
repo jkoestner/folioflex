6fb4a8a28adcad5ab718ce6b79aa2802180b5bb8
John Koestner <johnkoestner@outlook.com>
2019-08-18 13:45:51 -0400

    testing

--- pages/layouttab.py
+++ pages/layouttab.py
@@ -108,6 +108,6 @@ formatter_quote = {'avgTotalVolume':'{:,.2f}'.format,
                     'changePercent':'{0:.2%}'.format,
                     'ytdChange':'{0:.2%}'.format,}
 
-sector_list = ['XLF', 'XLU']
+sector_list = ['XLF', 'XLU', 'XLV', 'RWR']
                #, 'XLV', 'RWR', 'XLK', 'XLY', 'XLP', 'XLB', 'XLI', 'IYT']
 
