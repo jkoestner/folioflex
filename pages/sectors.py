2cb34fc42cb7ca566ea61da0da6c5e6cbd8af00c
John Koestner <johnkoestner@outlook.com>
2019-08-23 20:57:21 -0400

    reviewing what redistogo variable shows up

--- pages/sectors.py
+++ pages/sectors.py
@@ -32,7 +32,7 @@ layout = html.Div(
         
         html.Button(id='sector-initialize', children='Sector initialize'),
 
-        html.Div(id='refresh_text'),
+        html.Div(id='refresh_text', children=os.getenv('REDISTOGO_URL')),
         
 
         #graph
