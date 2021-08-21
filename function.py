a42225cd1fd7569c5787a0afa0fee2532c48c89e
John Koestner <johnkoestner@outlook.com>
2021-08-20 23:56:40 -0400

    update using yfinance

--- function.py
+++ function.py
@@ -1,15 +1,17 @@
 import pandas as pd
+import yfinance as yf
 from pages import layouttab
 
+
 def Query():
-    sector_close=pd.DataFrame([])
-    for i in layouttab.sector_list:
-        urlsec_data='https://api.worldtradingdata.com/api/v1/history?symbol=' + format(i) + '&sort=newest&api_token=aB0PKnbqXhFuYJtXmOvasDHf2M82BCY3PI9N9o4kb0UHwf5zVckMnD0PL2hc'
-        sector_temp = pd.read_json(urlsec_data, orient='columns')
-        sector_temp = pd.concat([sector_temp.drop(['history'], axis=1), sector_temp['history'].apply(pd.Series)], axis=1)
-        sector_temp["close"] = pd.to_numeric(sector_temp["close"])
-        sector_temp2 = sector_temp[['close']]
-        sector_temp2.columns = sector_temp.name.unique()
-        sector_close = pd.concat([sector_temp2, sector_close], axis=1)
-              
-    return sector_close
+    """Provides the sector historical stock prices
+
+    Returns
+    -------
+    sector_close : dataframe
+       provides the list of prices for historical prices
+    """
+
+    sector_close = yf.download(layouttab.sector_list, start="2018-01-01")
+
+    return sector_close["Adj Close"]
