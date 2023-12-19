69e99b7eec5a76dd77f70d7a0f80dd50a55dfa11
John Koestner <johnkoestner@outlook.com>
2023-12-19 00:34:38 -0500

    adding seleniumbase

--- folioflex/chatbot/scraper.py
+++ folioflex/chatbot/scraper.py
@@ -20,6 +20,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from seleniumbase import Driver
 from webdriver_manager.chrome import ChromeDriverManager
 
 from folioflex.utils import config_helper
@@ -31,7 +32,7 @@ logging.config.fileConfig(
 logger = logging.getLogger(__name__)
 
 
-def uc_create_options(location=None, extension=None):
+def uc_create_options(location=None, extension=None, **kwargs):
     """
     Create options for undetected_chromedriver (uc).
 
@@ -41,6 +42,8 @@ def uc_create_options(location=None, extension=None):
         location of the binary file for the browser
     extension : str (optional)
         location of the extension file for the browser that would like to load
+    **kwargs : dict (optional)
+        keyword arguments for the options of the driver
 
     Returns
     -------
@@ -67,7 +70,7 @@ def uc_create_options(location=None, extension=None):
     return options
 
 
-def create_driver(options, version=None, headless=False):
+def create_driver(options, version=None, headless=True):
     """
     Create the scraping driver for undetected_chromedriver (uc).
 
@@ -80,7 +83,7 @@ def create_driver(options, version=None, headless=False):
     version : int (optional)
         version of the ChromeDriver to use
     headless : bool (optional)
-        whether to run the driver in headless mode, default is False
+        whether to run the driver in headless mode, default is True
 
         note
         ----
@@ -103,7 +106,7 @@ def create_driver(options, version=None, headless=False):
                 correct_version = int(
                     e.msg.split("Current browser version is ")[1].split(".")[0]
                 )
-                print(f"using version {correct_version} of ChromeDriver")
+                logger.info(f"using version {correct_version} of ChromeDriver")
                 return uc.Chrome(options=options, version_main=correct_version)
             except Exception:
                 # couldn't parse correct version, raising same exception
@@ -112,7 +115,7 @@ def create_driver(options, version=None, headless=False):
             raise e from None
 
 
-def get_driver(driver="uc", **kwargs):
+def get_driver(driver="sb", **kwargs):
     """
     Get the scraping driver.
 
@@ -120,6 +123,7 @@ def get_driver(driver="uc", **kwargs):
     ----------
     driver : str (optional)
         driver to use, default is "uc"
+        - sb: selenium base
         - uc: undetected_chromedriver
         - selenium: selenium webdriver
     **kwargs : dict (optional)
@@ -135,6 +139,8 @@ def get_driver(driver="uc", **kwargs):
         return create_driver(options, **kwargs)
     elif driver == "selenium":
         return webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    elif driver == "sb":
+        return Driver(undetectable=True, **kwargs)
     else:
         raise ValueError(f"option {driver} not supported")
 
@@ -158,28 +164,15 @@ def scrape_html(url, **kwargs):
     # use the driver to get the valid html
     logger.info("initializing the driver")
     driver = get_driver(**kwargs)
-    driver.get(url)
-    time.sleep(2)  # wait for page to load
+    logger.info(f"loading {url}")
 
     if url.startswith("https://www.wsj.com/livecoverage/"):
         logger.info("wsj has specific landing page")
         url = "https://www.wsj.com/livecoverage/stock-market-today-dow-jones-12-11-2023"
-        driver.get(url)
+        driver.open(url)
+        driver.click('a:contains("Action")')
 
-        logger.info("capturing xpath and navigating to current page")
-        try:
-            xpath = "//a[span[text()=concat('Today', \"'s Action\")]]"
-            element = WebDriverWait(driver, 10).until(
-                EC.visibility_of_element_located((By.XPATH, xpath))
-            )
-            url_current = element.get_attribute("href")
-            driver.get(url_current)
-        except Exception:
-            logger.warning(f"could not find xpath, using {url}")
-            scrape_text = None
-            return scrape_text
-
-        logger.info(f"scraping {url_current}")
+        logger.info(f"scraping {driver.current_url}")
         time.sleep(3)  # wait for page to load
         html = driver.page_source
         soup = BeautifulSoup(html, "html.parser")
@@ -200,6 +193,9 @@ def scrape_html(url, **kwargs):
     # TODO: think about adding in https://www.bloomberg.com/ here
 
     else:
+        driver.open(url)
+        time.sleep(2)  # wait for page to load
+
         logger.info(f"scraping {url}")
         html = driver.page_source
         soup = BeautifulSoup(html, "html.parser")
