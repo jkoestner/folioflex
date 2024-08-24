1a6edb95c332c1d33c0df65d6079093507a49583
John Koestner <johnkoestner@outlook.com>
2024-08-24 01:59:11 -0400

    update dates

--- folioflex/portfolio/helper.py
+++ folioflex/portfolio/helper.py
@@ -12,12 +12,12 @@ import pandas as pd
 import pandas_market_calendars as mcal
 from dateutil.parser import parse
 
-from folioflex.utils import custom_logger
+from folioflex.utils import config_helper, custom_logger
 
 logger = custom_logger.setup_logging(__name__)
 
 
-def check_stock_dates(tx_df, fix=False, timezone="US/Eastern", warning=True):
+def check_stock_dates(tx_df, fix=False, warning=True, timezone="local"):
     """
     Check that the transaction dates are valid.
 
@@ -35,10 +35,10 @@ def check_stock_dates(tx_df, fix=False, timezone="US/Eastern", warning=True):
         transactions dataframe
     fix : bool (optional)
         if True then the dates will be fixed to previous valid date
-    timezone : str (optional)
-        timezone to use for checking dates
     warning : bool (optional)
         if True then a warning will be logged if dates are fixed
+    timezone : str (optional)
+        timezone to use for checking dates
 
     Returns
     -------
@@ -67,14 +67,22 @@ def check_stock_dates(tx_df, fix=False, timezone="US/Eastern", warning=True):
             "The date column is not a date object, please convert it to a date object"
         )
 
+    # get the timezone
+    if timezone == "local":
+        timezone = config_helper.LOCAL_TIMEZONE
+
     # date checks
-    tx_df_min = tx_df_min = tx_df["date"].min() - timedelta(days=7)
+    tx_df_min = tx_df["date"].min() - timedelta(days=7)
     tx_df_max = tx_df["date"].max()
     stock_dates = mcal.get_calendar("NYSE").schedule(
         start_date=tx_df_min, end_date=tx_df_max
     )
-    stock_dates["market_open"] = stock_dates["market_open"].dt.tz_convert(timezone)
-    stock_dates["market_close"] = stock_dates["market_close"].dt.tz_convert(timezone)
+    stock_dates["market_open"] = convert_date_to_timezone(
+        stock_dates["market_open"], timezone
+    )
+    stock_dates["market_close"] = convert_date_to_timezone(
+        stock_dates["market_close"], timezone
+    )
     stock_dates = pd.to_datetime(stock_dates["market_open"]).dt.date
 
     # change datetime to date
@@ -110,6 +118,37 @@ def check_stock_dates(tx_df, fix=False, timezone="US/Eastern", warning=True):
     return {"invalid_dt": invalid_dt, "fix_tx_df": fix_tx_df}
 
 
+def convert_date_to_timezone(date_series, timezone="local"):
+    """
+    Convert date column to a specific timezone.
+
+    Parameters
+    ----------
+    date_series : series
+        series with date column
+    timezone : str (optional)
+        timezone to convert the date to
+
+    Returns
+    -------
+    converted_date : series
+        series with date column converted to timezone
+
+    """
+    if not isinstance(date_series, pd.Series):
+        raise ValueError("date_series must be a pandas Series")
+
+    if timezone == "local":
+        timezone = config_helper.LOCAL_TIMEZONE
+    elif timezone is None and date_series.dt.tz:
+        converted_date = pd.to_datetime(date_series).dt.tz_localize(timezone)
+    elif date_series.dt.tz:
+        converted_date = pd.to_datetime(date_series).dt.tz_convert(timezone)
+    else:
+        converted_date = pd.to_datetime(date_series).dt.tz_localize(timezone)
+    return converted_date
+
+
 def most_recent_stock_date():
     """Get the most recent stock date."""
     stock_dates = mcal.get_calendar("NYSE").schedule(
@@ -133,6 +172,7 @@ def prettify_dataframe(dataframe):
     -------
     DataFrame
         prettified dataframe
+
     """
     if not isinstance(dataframe, pd.DataFrame):
         raise ValueError("dataframe must be a pandas DataFrame")
@@ -159,6 +199,7 @@ def convert_lookback(lookback):
     -------
     converted_lookback : int
         converted lookback
+
     """
 
     def is_string_a_date(string):
