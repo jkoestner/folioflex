7a659252dda2158e5e72ae13409a269f58a090d2
John Koestner <john_dnr@outlook.com>
2026-09-12 15:03:02 -0400

    add updated versions and uv.lock file
    fix g4f changes

--- folioflex/chatbot/providers.py
+++ folioflex/chatbot/providers.py
@@ -112,7 +112,7 @@ class G4FProvider(ChatBotProvider):
     """
 
     def get_chatbot(
-        self, g4f_provider=g4f.Provider.bing, auth=False, access_token=None
+        self, g4f_provider=g4f.Provider.BaseProvider, auth=False, access_token=None
     ):
         """
         G4F chatbot.
